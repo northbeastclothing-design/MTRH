25486ea2159c98b248f80c44b4213c134c3065be
Jesse Huffman <jessehuffman@Jesses-Freelance-Mac.local>
2026-06-04 15:00:30 -0600

    style: set onboarding arrow offsets for step 4 and step 8 to user values

--- src/App.tsx
+++ src/App.tsx
@@ -9588,7 +9588,7 @@ function App() {
                     return {
                       ...common,
                       bottom: '-10px',
-                      left: '202px',
+                      left: '150px',
                       borderWidth: '10px 8px 0 8px',
                       borderColor: `${tooltipTheme.bg} transparent transparent transparent`,
                     };
@@ -9596,7 +9596,7 @@ function App() {
                     return {
                       ...common,
                       top: '-10px',
-                      right: '84px',
+                      right: '110px',
                       borderWidth: '0 8px 10px 8px',
                       borderColor: `transparent transparent ${tooltipTheme.bg} transparent`,
                     };
